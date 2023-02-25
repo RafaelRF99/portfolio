ea55334cd1bc309ab25fb30d501eaffabc6116fc
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-02-24 21:08:01 -0300

    NavBar oculto

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -3,7 +3,7 @@ import styles from './NavBar.module.css'
 export default function NavBar() {
     return (
         <div className={styles.container}>
-            <h1>NavBar</h1>
+            <h1></h1>
         </div>
     )
 }
