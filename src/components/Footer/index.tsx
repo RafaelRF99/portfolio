727f9a275a68a6a43ff4659da703afab6f3c8818
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-02-28 19:29:48 -0300

    Footer add target blank

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -12,11 +12,15 @@ export default function Footer() {
             <div className={styles.contato}>
                 <h1 className={styles.title}>Contato</h1>
                 <div className={styles.link}>
-                    <Link passHref href="mailto: rafaelfferreira99@hotmail.com"><MdEmail /><span>Email</span></Link>
-                    <Link passHref href="https://wa.me/5519998255728"><BsWhatsapp /><span>WhatsApp</span></Link>
-                    <Link passHref href="https://www.linkedin.com/in/rafael-ferreira99/"><VscAccount />
+                    <Link passHref target="_blank" 
+                     href="mailto: rafaelfferreira99@hotmail.com"><MdEmail /><span>Email</span></Link>
+                    <Link passHref target="_blank" 
+                     href="https://wa.me/5519998255728"><BsWhatsapp /><span>WhatsApp</span></Link>
+                    <Link passHref target="_blank" 
+                     href="https://www.linkedin.com/in/rafael-ferreira99/"><VscAccount />
                         <span>LinkedIn</span></Link>
-                    <Link passHref href="https://github.com/RafaelRF99"><BsGithub /><span>GitHub</span></Link>
+                    <Link passHref target="_blank" 
+                     href="https://github.com/RafaelRF99"><BsGithub /><span>GitHub</span></Link>
                 </div>
             </div>
             <div className={styles.honra}>
