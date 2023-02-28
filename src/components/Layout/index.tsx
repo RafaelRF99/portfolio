fb05b8a552e19a31bf79e5a10c268582cb88e643
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-02-28 20:41:15 -0300

    Layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,30 @@
+import Banner from "../Banner";
+import Educacao from "../Educacao";
+import Experiencia from "../Experiencia";
+import Habilidade from "../Habilidade";
+import Portfolio from "../Portfolio";
+import Sobre from "../Sobre";
+import Head from 'next/head'
+import NavBar from "../NavBar";
+import Footer from "../Footer";
+
+export default function Layout() {
+    return (
+        <>
+            <Head>
+                <link rel="stylesheet" href="/icons/Angular.png" />
+                <title>Rafael Ferreira</title>
+            </Head>
+            <main>
+                <NavBar />
+                <Banner />
+                <Sobre />
+                <Experiencia nome="Experiência" />
+                <Educacao titulo="Formação" cursos="Cursos" />
+                <Habilidade />
+                <Portfolio />
+            </main>
+            <Footer />
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,10 @@
-import Banner from '../components/Banner'
-import Educacao from '../components/Educacao'
-import Experiencia from '../components/Experiencia'
-import Footer from '../components/Footer'
-import Habilidade from '../components/Habilidade'
-import NavBar from '../components/NavBar'
-import Portfolio from '../components/Portfolio'
-import Sobre from '../components/Sobre'
+import Layout from '../components/Layout'
 
 export default function Home() {
 
   return (
     <>
-      <NavBar />
-      <Banner />
-      <Sobre />
-      <Experiencia nome="Experiência" />
-      <Educacao titulo="Formação" cursos="Cursos" />
-      <Habilidade />
-      <Portfolio />
-      <Footer />
+      <Layout />
     </>
   )
 }
