d8664180f3d9ebe439b436fdaf391669a671bb95
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-09-27 15:08:00 -0300

    feat: banner dark and light

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,13 @@
+import { useTheme } from '../../data/hooks/useTheme'
 import styles from './Banner.module.css'
 import Image from 'next/image'
 
 export default function Banner() {
+    const { theme } = useTheme()
+    const isBanner = theme === 'dark' ? styles.banner : styles.banner2
 
     return (
-        <div className={styles.container}>
+        <div className={`${styles.container} ${isBanner}`}>
             <Image className={styles.img} src="/img/Rafael.jpeg" alt='Rafael' width={180} height={180} 
             priority={10 === 10}/>
             <h1 className={styles.title}>Rafael Ferreira</h1>
