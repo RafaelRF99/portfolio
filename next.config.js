47762b066f4758a8559847e29ef73df287af1b2f
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-09-28 08:32:29 -0300

    fix: banner img

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
+  images: {
+    formats: ['image/jpeg', 'image/png', 'image/webp'],
+  },
 }
 
 module.exports = nextConfig
